eff3c17cec6228eea1c3d615adc4b9a4772a4733
Daniel Jose Doria Del Castillo <39770697+danieljosedx@users.noreply.github.com>
2023-05-22 20:33:31 -0500

    Initial commit

---
+++ js/script2.js
@@ -0,0 +1,28 @@
+function showAlert() { alert("¡Hola, esta es una alerta desde JavaScript!");}
+
+// Agregar un oyente de eventos al formulario para manejar el evento “submit”:
+const form = document.getElementById('myForm');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  validateForm();
+});
+
+// Sumar una función de validación que verifique los criterios deseados:
+function validateEmail(email) {
+    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+  }
+  function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+    if (!validateEmail(email)) {
+      alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+      alert('Datos enviados correctamente.');
+    }
+  }
+
+document.querySelector("button.button-menu-toggle")
+  .addEventListener("click", function() {
+         document.querySelector(".nav-links").
+                    classList.toggle("nav-links-responsive")})
